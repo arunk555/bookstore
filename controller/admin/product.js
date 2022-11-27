626ef5a328db15a98a3eef33001f148ff060df3a
arunk555 <arunkurusev@gmail.com>
2022-11-27 14:49:28 +0000

    latest changes

--- controller/admin/product.js
+++ controller/admin/product.js
@@ -12,6 +12,25 @@ const allproducts=async (req, res)=>{
     }
 };
 
+const add=async (req, res)=>{
+    const {name, price } = req.body; 
+    if(!( name && price)) throw new Error("Mandatory fields are missing");
+    const prodobj = new Product({name, price });
+    try {
+      const isAlready = await Product.findOne({name});
+      if(isAlready)  throw new Error("Product already exists");
+      
+
+      return res.status(200).json(allprod);
+    } catch (error) {
+       return res.status(400).json({
+        success: false,
+        message: error.message
+       });
+    }
+};
+
+
 
 
 
